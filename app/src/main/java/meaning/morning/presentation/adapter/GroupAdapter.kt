40c0b42ce704cb554cc1301e09243b8553ee5dea
hyooosong <gythd1998@gmail.com>
2021-01-05 13:49:04 +0900

    [refactor] 그룹 관련 어댑터 수정
    코드리뷰 반영, 주석 추가
    
    Related to: #10

--- app/src/main/java/meaning/morning/presentation/adapter/GroupAdapter.kt
+++ app/src/main/java/meaning/morning/presentation/adapter/GroupAdapter.kt
@@ -1,21 +1,24 @@
+/*
+ * Created By: hyooosong
+ * on 2021.01.05
+ */
+
 package meaning.morning.presentation.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import meaning.morning.data.GroupData
 import meaning.morning.databinding.ItemGroupListBinding
 
-class GroupAdapter (private val context: Context) : RecyclerView.Adapter<GroupAdapter.VHolder>() {
-    var data = listOf<GroupData>()
-    private lateinit var binding: ItemGroupListBinding
+class GroupAdapter : RecyclerView.Adapter<GroupAdapter.VHolder>() {
+    private val data: MutableList<GroupData> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VHolder {
-        binding = ItemGroupListBinding.inflate(
-            LayoutInflater.from(context), parent, false
-        )
-        return VHolder()
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ItemGroupListBinding
+            .inflate(layoutInflater, parent, false)
+            .let { VHolder(it) }
     }
 
     override fun onBindViewHolder(holder: VHolder, position: Int) {
@@ -24,7 +27,15 @@ class GroupAdapter (private val context: Context) : RecyclerView.Adapter<GroupAd
 
     override fun getItemCount(): Int = data.size
 
-    inner class VHolder : RecyclerView.ViewHolder(binding.root) {
+    fun refreshData(list: List<GroupData>) {
+        data.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class VHolder(
+        private val binding: ItemGroupListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
         fun onBind(data: GroupData) {
             binding.group = data
         }
