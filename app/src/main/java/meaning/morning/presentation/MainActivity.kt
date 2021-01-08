e7df302f872cf71e71ff857cefab993ff5c4fdae
LEE-HYUNGJUN <72328789+LEE-HYUNGJUN@users.noreply.github.com>
2021-01-08 15:29:09 +0900

    [inflate] 홈 메인 페인지 inflate 작업 완료 (#20)
    * [feat] 바텀 내비게이션 수정
    
    바텀내비게이션 클릭시 색 수정
    
    Related to : #17
    
    * [feat] 홈 메인페이지 레이아웃 작성
    
    홈 메인페이지 카드뷰 추가
    
    Related to : #17
    
    * [refactor] 홈 액티비티 코드 수정
    
    실행시 홈에 바로 프래그먼트 띄움
    
    Related to : #17
    
    * [refacor] 홈 메인 페이지 패키징 및 코드 수정
    
    홈 메인 페이지 패키징 작업을 수행. 어댑터와 액티비티에서 코드 수정
    
    Related to : #19
    
    * [feat] 리사이클러뷰 아이템 간격 조절 클래스 추가
    
    아이템 리스트를 조정하는 클래스를 추가. 어댑터에서 주석 제거
    
    Related to : #19
    
    * [refactor] 홈 메인페이지 레이아웃 수정
    
    홈 메인 페이지 레이아웃 수정
    
    Related to : #17
    
    * [refactor] 홈 메인 페이지 layout 수정
    
    홈 메인 페이지 layout 수정 및 color값 수정
    
    Related to : #17
    
    * [feat] 홈 메인 페이지 카드 애니메이션 기능 추가
    
    홈 메인 페이지 카드 애니메이션 적용. 
    
    Related to : #19

---
+++ app/src/main/java/meaning/morning/presentation/MainActivity.kt
@@ -0,0 +1,53 @@
+package meaning.morning.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import meaning.morning.R
+import meaning.morning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val homeFragment = HomeFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this
+
+
+        changeFragment(homeFragment)
+        initNavigationBar()
+    }
+
+    private fun initNavigationBar(){
+        binding.bottomNavigation.run {
+            setOnNavigationItemSelectedListener {
+               when(it.itemId){
+                   R.id.bottom_bar_home ->{
+                       changeFragment(homeFragment)
+                       return@setOnNavigationItemSelectedListener true
+                   }
+                   R.id.bottom_bar_group -> {
+                       val fragment = GroupFragment()
+                       changeFragment(fragment)
+                       return@setOnNavigationItemSelectedListener  true
+                   }
+                   else -> false
+               }
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout,fragment)
+            .commit()
+    }
+}
