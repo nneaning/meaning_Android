35a57d56775586517c337bc021c0295a5fd76a44
hyooosong <72931738+hyooosong@users.noreply.github.com>
2021-01-08 15:20:04 +0900

    [feat] 그룹 생성 뷰 이벤트 추가 (#23)
    토스트 메시지 출력, 인원수에 따른 label 색상 변경
    
    Related to: #18

--- app/src/main/java/meaning/morning/presentation/GroupFragment.kt
+++ app/src/main/java/meaning/morning/presentation/GroupFragment.kt
@@ -7,11 +7,11 @@ package meaning.morning.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import meaning.morning.*
@@ -45,6 +45,7 @@ class GroupFragment : Fragment() {
         binding.imageviewAddGroup.setOnClickListener {
             val intent = Intent(activity, AddGroupActivity::class.java)
             startActivity(intent)
+
         }
     }
 
