0c658485c95cd0ed23e1533174d0b13cc2984d82
hyooosong <gythd1998@gmail.com>
2021-01-09 00:36:44 +0900

    [upload] 그룹 가입 승인, 거절 Dialog 추가
    myGroup visibility에 따라 Label 설정
    
    Related to: #24

--- app/src/main/java/meaning/morning/BindingDialog.kt
+++ app/src/main/java/meaning/morning/BindingDialog.kt
@@ -6,22 +6,31 @@ package meaning.morning
 
 import android.app.Dialog
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
-import android.widget.ImageView
+import android.view.View
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import meaning.morning.data.GroupDetailData
 import meaning.morning.databinding.DialogGroupDetailBinding
+import meaning.morning.databinding.DialogGroupRecyclerBinding
+import meaning.morning.databinding.FragmentGroupBinding
 
 class BindingDialog(private val context: Context) {
-    private lateinit var dialogBinding: DialogGroupDetailBinding
+    private val binding: FragmentGroupBinding = DataBindingUtil
+        .inflate(LayoutInflater.from(context), R.layout.fragment_group, null, false)
     private var dialog = Dialog(context)
+    private var childDialog = Dialog(context)
     fun showDialog() {
-        dialogBinding = DataBindingUtil
+        val dialogBinding: DialogGroupDetailBinding = DataBindingUtil
             .inflate(LayoutInflater.from(context), R.layout.dialog_group_detail, null, false)
         dialog.setCancelable(false)
         dialog.setContentView(dialogBinding.root)
+
         setGroupData(dialogBinding)
         closeDialog(dialogBinding.imageviewClose)
+        showApproveDialog(dialogBinding.textviewJoinBtn)
+
         dialog.show()
     }
 
@@ -30,12 +39,46 @@ class BindingDialog(private val context: Context) {
             "송이 좋아하는 그룹",
             "취준생끼리취준생끼리취준생끼리 \n취준생끼리취준생끼리취준생끼리 \n취준생끼리취준생끼리취준생끼리",
             "2",
-            "/5")
+            "/5"
+        )
     }
 
-    private fun closeDialog(closeBtn: ImageView) {
+    private fun closeDialog(closeBtn: View) {
         closeBtn.setOnClickListener {
             dialog.dismiss()
+            childDialog.dismiss()
+        }
+    }
+
+    private fun checkMyGroup(): Boolean = binding.layoutMyGroup.visibility == View.VISIBLE
+
+    private fun showChildDialog() {
+        val recyclerDialogBinding: DialogGroupRecyclerBinding = DataBindingUtil
+            .inflate(LayoutInflater.from(context), R.layout.dialog_group_recycler, null, false)
+        childDialog.setCancelable(false)
+
+        childDialog.setContentView(recyclerDialogBinding.root)
+        setTextDialog(recyclerDialogBinding)
+        closeDialog(recyclerDialogBinding.imageviewClose)
+        closeDialog(recyclerDialogBinding.textviewOkBtn)
+        childDialog.show()
+    }
+
+    private fun setTextDialog(binding: DialogGroupRecyclerBinding) {
+        var dialogLabel = "default"
+        if (checkMyGroup())
+            dialogLabel = "이미 함께 하고 있는 그룹이 있어요!"
+
+        if (!checkMyGroup())
+            dialogLabel = "그룹 참가가 완료되었습니다."
+
+        binding.textviewDialogLabel.text = dialogLabel
+    }
+
+    private fun showApproveDialog(joinBtn: TextView) {
+        joinBtn.setOnClickListener {
+            dialog.dismiss()
+            showChildDialog()
         }
     }
 }
