8726cac3ddb6fac44bb55c089bc892fef6544d1e
leehj1232 <leehj1232@naver.com>
2021-01-08 21:57:22 +0900

    [feat] 홈 카드 클릭 시 레이아웃 작업
    홈 메인 페이지에서 카드 클릭 시 레이아웃 작업 완료
    
    Related to : #25

---
+++ app/src/main/java/meaning/morning/presentation/GroupFragment.kt
@@ -0,0 +1,20 @@
+package meaning.morning.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import meaning.morning.R
+
+
+class GroupFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_group, container, false)
+    }
+
+}
---
+++ app/src/main/java/meaning/morning/presentation/HomeFragment.kt
@@ -0,0 +1,20 @@
+package meaning.morning.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import meaning.morning.R
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
