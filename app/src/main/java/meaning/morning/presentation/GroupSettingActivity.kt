491e0dea1bc0675bd22303602c3ef78462e2ab9d
hyooosong <72931738+hyooosong@users.noreply.github.com>
2021-01-11 04:23:48 +0900

    [feat] inflate 그룹 설정 레이아웃 (#32)
    * [feat] inflate 그룹 설정 레이아웃
    
    recyclerview 더미 데이터 확인, 사용자 프로필 짝수 position 구분
    
    Related to: #31
    
    * [refactor] 파일 주석 생성

--- app/src/main/java/meaning/morning/presentation/GroupSettingActivity.kt
+++ app/src/main/java/meaning/morning/presentation/GroupSettingActivity.kt
@@ -1,12 +1,98 @@
+/*
+ * Created By: hyooosong
+ * on 2021.01.10
+ */
 package meaning.morning.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import meaning.morning.R
+import meaning.morning.data.GroupMemberData
+import meaning.morning.databinding.ActivityGroupSettingBinding
+import meaning.morning.presentation.adapter.GroupSettingAdapter
 
 class GroupSettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGroupSettingBinding
+    private lateinit var settingAdapter: GroupSettingAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group_setting)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_group_setting)
+        setGroupSettingAdapter()
+        loadGroupMember()
+    }
+
+    private fun setGroupSettingAdapter() {
+        settingAdapter = GroupSettingAdapter()
+        binding.rcvGroupMember.apply {
+            adapter = settingAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun loadGroupMember() {
+        var memberData = mutableListOf<GroupMemberData>()
+
+        memberData.apply {
+            add(
+                GroupMemberData(
+                    "박효송",
+                    "매일 8시 기상",
+                    "30일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "박진수",
+                    "매일 5시 기상",
+                    "20일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "이형준",
+                    "매일 6시 기상",
+                    "104일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "미닝",
+                    "매일 4시 기상",
+                    "110일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "미닝",
+                    "매일 4시 기상",
+                    "110일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "미닝",
+                    "매일 4시 기상",
+                    "110일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "미닝",
+                    "매일 4시 기상",
+                    "110일째 진행중"
+                )
+            )
+            add(
+                GroupMemberData(
+                    "미닝",
+                    "매일 4시 기상",
+                    "110일째 진행중"
+                )
+            )
+        }
+        settingAdapter.refreshData(memberData)
     }
 }
