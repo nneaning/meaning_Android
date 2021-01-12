4c67cfa443c17e2652c4ed353d4dea9aa4ce6736
hyooosong <gythd1998@gmail.com>
2021-01-12 17:32:04 +0900

    [feat] 스플래시 뼈대 작성

---
+++ app/src/main/java/meaning/morning/SplashActivity.kt
@@ -0,0 +1,28 @@
+package meaning.morning
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import meaning.morning.databinding.ActivitySplashBinding
+import meaning.morning.presentation.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivitySplashBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        binding.meaningSplash.playAnimation()
+        loadSplash()
+    }
+
+    private fun loadSplash() {
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
