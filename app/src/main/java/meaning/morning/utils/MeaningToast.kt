a862ec2a69fa1a9539fb723c5f643056faf1ad28
jinsu4755 <parkjinsu4755@gmail.com>
2021-01-12 12:28:23 +0900

    [fix] custom Toast 확장성 추가
    1. 굳이 시간까지 받을 필요는 없어서 생성자 하나 추가시킴

--- app/src/main/java/meaning/morning/utils/MeaningToast.kt
+++ app/src/main/java/meaning/morning/utils/MeaningToast.kt
@@ -3,6 +3,7 @@ package meaning.morning.utils
 import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import meaning.morning.R
@@ -17,17 +18,24 @@ class MeaningToast(
     init {
         toast = Toast.makeText(context, message, duration)
         val view = getToastView(context)
-        view.findViewById<TextView>(R.id.toast_message).apply {
-            text = message
-        }
+        view.findViewById<TextView>(R.id.toast_message).apply { text = message }
+        setToastView(view)
+    }
+
+    constructor(
+        context: Context,
+        message: String
+    ) : this(context, message, Toast.LENGTH_SHORT)
+
+    private fun getToastView(context: Context) = LayoutInflater.from(context)
+        .inflate(R.layout.toast_gray4_bd, null)
+
+    private fun setToastView(view: View) {
         toast?.let {
             it.setGravity(Gravity.CENTER, 0, 550)
             it.setView(view)
         }
     }
-
-    private fun getToastView(context: Context) = LayoutInflater.from(context)
-        .inflate(R.layout.toast_gray4_bd, null)
 
     fun showToast() {
         toast?.show()
