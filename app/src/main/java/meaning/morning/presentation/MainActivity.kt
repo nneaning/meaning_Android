c3a1991519885d85e3ac0f150b5377e96c922cec
hyooosong <72931738+hyooosong@users.noreply.github.com>
2021-01-12 15:00:30 +0900

    [feat] sharedPreferences 싱글톤 작성 (#34)
    * [style] 폰트 삭제, 그룹 카드 뷰 테두리 수정
    
    경량화 위해 notosan 폰트 삭제
    
    * [feat] sharedPreferences 싱글톤 작성
    
    sharedPreferences 작성, addGroupData 로 확인

--- app/src/main/java/meaning/morning/presentation/MainActivity.kt
+++ app/src/main/java/meaning/morning/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
 package meaning.morning.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import meaning.morning.R
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.run {
             setOnNavigationItemSelectedListener {
                when(it.itemId){
-                   R.id.bottom_bar_home ->{
+                   R.id.bottom_bar_home -> {
                        val fragment = HomeFragment()
                        changeFragment(fragment)
                        return@setOnNavigationItemSelectedListener true
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
                    R.id.bottom_bar_group -> {
                        val fragment = GroupFragment()
                        changeFragment(fragment)
-                       return@setOnNavigationItemSelectedListener  true
+                       return@setOnNavigationItemSelectedListener true
                    }
                    else -> false
                }
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     private fun changeFragment(fragment: Fragment){
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.frame_layout,fragment)
+            .replace(R.id.frame_layout, fragment)
             .commit()
     }
 }
