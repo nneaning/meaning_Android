143bbebfb3ee4f416108dc2931d4c6922049f1aa
Jinsu Park <parkjinsu4755@gmail.com>
2021-01-12 14:30:28 +0900

    [feat] Inflate camera 카메라 프리뷰 화면 구현 (#36)
    * [feat] 타임스템프 카메라 엑티비티
    
    * [fix] 폰트 수정
    
    * [feat] 타임스템프 카메라 프리뷰 구현
    
    1.cameraX를 이용하여 surfaceView PreView를 구현하였고 해당 뷰는 UI Thread에서 작동
    2.PreView화면 위에 시간이 실시간으로 보여야하기 때문에 해당 부분 LiveData를 사용하여 전달
    3.사진을 찍으면 imageProxy를 Bitmap으로 바꾸는 callBack 작성

--- app/src/main/java/meaning/morning/presentation/camera/TimeStampCameraActivity.kt
+++ app/src/main/java/meaning/morning/presentation/camera/TimeStampCameraActivity.kt
@@ -1,17 +1,118 @@
 package meaning.morning.presentation.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import meaning.morning.R
 import meaning.morning.databinding.ActivityTimeStampCameraBinding
 
-class TimeStampCameraActivity:AppCompatActivity() {
+class TimeStampCameraActivity : AppCompatActivity() {
+
+    private val cameraViewModel: CameraViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding:ActivityTimeStampCameraBinding = DataBindingUtil
+        val binding: ActivityTimeStampCameraBinding = DataBindingUtil
             .setContentView(this, R.layout.activity_time_stamp_camera)
         binding.lifecycleOwner = this
+        initTimeStampCamera()
+    }
+
+    private fun initTimeStampCamera() {
+        if (allPermissionGranted()) {
+            loadCameraView()
+            return
+        }
+        requestPermission()
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(applicationContext,it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            REQUIRED_PERMISSIONS,
+            CameraViewModel.REQUEST_CODE_PERMISSIONS
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CameraViewModel.REQUEST_CODE_PERMISSIONS) {
+            permissionResponseEvent()
+        }
+    }
+
+    private fun permissionResponseEvent() {
+        if (allPermissionGranted()) {
+            loadCameraView()
+            return
+        }
+        permissionDeniedEvent()
+    }
+
+    private fun permissionDeniedEvent() {
+        //TODO 커스텀 토스트로 변경하기.
+        Toast.makeText(
+            this,
+            "권한을 승인하지 않으면 당신의 미라클 모닝을 기록할 수 없어요!",
+            Toast.LENGTH_LONG
+        ).show()
+        finish()
+    }
+
+    private fun loadCameraView() {
+        changeFragment(CameraFragment())
+        observeCancelButton()
+    }
+
+    private fun changeFragment(initFragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.apply {
+            replace(R.id.fragment_camera, initFragment)
+            commit()
+        }
+    }
+
+    fun changeFragment(changeFragment: Fragment, backStackName: String?) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.apply {
+            setCustomAnimations(
+                R.anim.enter_from_right_fragment,
+                R.anim.fragment_fade_exit,
+                R.anim.fragment_open_enter,
+                R.anim.fragment_close_exit
+            )
+            replace(R.id.fragment_camera, changeFragment)
+            addToBackStack(backStackName)
+            commit()
+        }
+    }
+
+    private fun observeCancelButton() {
+        cameraViewModel.isCameraCancel.observe(this){
+            if (it) {
+                onBackPressed()
+                cameraViewModel.enableCameraCancelEvent()
+            }
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS =
+            arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 
 }
