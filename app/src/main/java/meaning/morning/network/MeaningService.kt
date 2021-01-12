c16052984d77a3d45c39cdf8caeab9af4c912784
Jinsu Park <parkjinsu4755@gmail.com>
2021-01-12 15:14:38 +0900

    [feat] 서버 연결로직 작성 (#43)
    1. 서버와 통신하기 위한 service interface 작성
    2. baseUrl 감춤
    3. service 구현체는 싱글턴 방식으로 멀티스레드 세이프티하게 던져줌

---
+++ app/src/main/java/meaning/morning/network/MeaningService.kt
@@ -0,0 +1,25 @@
+/*
+ * Created by jinsu4755
+ * DESC: 서버 연결 interface
+ * MeaningService.getInstance().apiMethod 로 사용이 가능하다
+ */
+
+package meaning.morning.network
+
+interface MeaningService {
+    /*TODO 서버 연결을 위한 함수는 여기
+    * Ex)
+    * @Annotaion
+    * fun request0000():Call<BaseResponse<DataClass or any >>
+    * */
+
+    companion object {
+        @Volatile
+        private var instance: MeaningService? = null
+
+        fun getInstance(): MeaningService = instance ?: synchronized(this) {
+            instance ?: provideService(MeaningService::class.java)
+                .apply { instance = this }
+        }
+    }
+}
